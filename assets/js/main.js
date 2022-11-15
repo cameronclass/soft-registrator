cf3ad229b1d1d8d375b7cf380acf962a6ccd94de
cameronclass <kamrankutumov@gmail.com>
2022-11-15 11:05:53 +0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,96 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  let cardsSlider = new Swiper(".d-page__cards_content.swiper", {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
 
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        direction: "horizontal",
+        centeredSlides: true,
+      },
+      576: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+        direction: "horizontal",
+        centeredSlides: false,
+      },
+      1200: {
+        direction: "vertical",
+        slidesPerView: "auto",
+        loop: false,
+        centeredSlides: false,
+      },
+    },
+  });
+
+  new Swiper(".d-page__content_gallery_slider .swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        centeredSlides: true,
+      },
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+    navigation: {
+      nextEl: ".d-page__content_gallery_slider .swiper-button-next",
+      prevEl: ".d-page__content_gallery_slider .swiper-button-prev",
+    },
+  });
+
+  /* function reportWindowSize() {
+    let width = window.innerWidth;
+    if (width < 1201) {
+      cardsSlider.destroy();
+      cardsSlider.init(cardsSlider);
+    }
+  }
+
+  window.addEventListener("resize", function () {
+    reportWindowSize();
+  }); */
+
+  const tabsUniversal = document.querySelector(".tabs");
+  const tabsContentUniversal = document.querySelectorAll(".tab-content");
+
+  tabsUniversal.addEventListener("click", (event) => {
+    let currentTabTarget = event.target;
+    let currentTab = event.target.dataset.tab;
+
+    for (let i = 0; i < tabsUniversal.children.length; i++) {
+      const element = tabsUniversal.children[i];
+      element.classList.remove("active");
+    }
+
+    tabsContentUniversal.forEach((item) => {
+      if (item.classList.contains("active")) {
+        item.classList.remove("active");
+      } else {
+        console.log("error");
+      }
+
+      currentTabTarget.classList.add("active");
+      if (item.dataset.tabContent == currentTab) {
+        item.classList.add("active");
+      }
+    });
+  });
+});
